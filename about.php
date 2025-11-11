917045c72d0e20cfdea5bb7eeecd7453a40e3818
13ernice <niceyxe@gmail.com>
2025-11-11 13:49:35 +0800

    edit aboutus css

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="aboutus.css">
+</head>
+<body>
+    <div class="hero-img1">
+        <div class="hero-text">
+            <h1>About This Web</h1>
+        </div>
+    </div>
+
+    <div>
+        <h3 class="para1">
+            This MiniMart Manager system is designed to assist store staff in efficientky monitor and manage product inveentory.
+            <br>Inventory management plays a role in ensuring that stock levels are accurate, products are available for customers, and the business runs smoothly.
+            <br>This system provides a simple and user-friendly interface that allows staff to <br>add new products, view current stock levels and update item details and delete item that no longer available.
+        </h3>
+    </div>
+
+    <div class="hero-img2">
+        <div class="hero-text">
+            <h1>Meet Our Team</h1>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            
+        </div>
+    </div>
+</body>
+</html>
