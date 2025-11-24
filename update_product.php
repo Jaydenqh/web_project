6bb1c218dc6f5869c97e32979c2525803e0e59f9
jayden <fv95cz94fm@privaterelay.appleid.com>
2025-11-24 16:30:44 +0800

    final

--- update_product.php
+++ update_product.php
@@ -32,7 +32,7 @@ if (isset($_POST['update'])) {
 <head>
   <meta charset="UTF-8">
   <title>Update Product | MiniMart Manager</title>
-  <link rel="stylesheet" href="update.css">
+  <link rel="stylesheet" href="CSS/update.css">
 </head>
 <body>
   <header>
